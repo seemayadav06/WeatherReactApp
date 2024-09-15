2ea161ba6108d1173d61257f95dadc5cfcb22d12
Dsa-code <seeya.sy327@gmail.com>
2024-09-15 18:59:02 +0530

    first commit

---
+++ weatherApp/src/App.jsx
@@ -0,0 +1,13 @@
+//https://api.openweathermap.org/data/2.5/weather?q=lucknow&appid=d0427351685bccb69fe7397d846a8bac
+import React from 'react'
+import Weather from './components/Weather'
+
+const App=()=>{
+  return (
+    
+    <div className='app'>
+      <Weather></Weather>
+    </div>
+  )
+}
+export default App
